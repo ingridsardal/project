f085d2b3b4cc5dd037265ad87cf18bf7114d9776
MaxBergstrom123 <maxb.bergstrom@gmail.com>
2024-01-05 17:25:47 +0100

    till david

--- src/views/CreatorLeaderboardView.vue
+++ src/views/CreatorLeaderboardView.vue
@@ -403,7 +403,6 @@ export default {
   display: none;
 }
 
-<<<<<<< HEAD
 @media screen and (max-width:50em) {
 
   .message {
@@ -436,9 +435,5 @@ export default {
 
 
 }
-=======
-.message{ font-size: 20px;
-margin-top: 25%;}
->>>>>>> cca03f15f77b4c151fd75a349416a061d762ed47
 
 </style>
