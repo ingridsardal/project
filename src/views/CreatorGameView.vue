19c49a6b3d6e5a89fe3a9328cc7e5e16b8bfe2d9
Anna Sparr <annasparr@student-210-116.eduroam.uu.se>
2024-01-06 11:26:30 +0100

    font

--- src/views/CreatorGameView.vue
+++ src/views/CreatorGameView.vue
@@ -1,31 +1,34 @@
 <template>
   <div>
+
     <body class="background">
-    <header>
-      <h1> {{uiLabels.giveScore}} </h1>
-      <h3  style="font-weight: bold;">{{uiLabels.roundNumber}} {{ roundCounter }}, {{uiLabels.letter}}{{ selectedLetter.toUpperCase() }}</h3>
-    </header>
-      <h3>{{uiLabels.markScore}} </h3>
+      <header>
+        <h1> {{ uiLabels.giveScore }} </h1>
+        <h3 style="font-weight: bold;">{{ uiLabels.roundNumber }} {{ roundCounter }}, {{ uiLabels.letter }}{{
+          selectedLetter.toUpperCase() }}</h3>
+      </header>
+      <h3>{{ uiLabels.markScore }} </h3>
 
       <section id="Section1">
         <div class="wrapper">
 
           <div v-for="player in players" :key="player.id" class="player-item">
-            <h2>{{ player.nameId }} 
-              <div id="playerAvatar"> 
-                <img :src="player.avatar" alt="Animated GIF" >
+            <h2>{{ player.nameId }}
+              <div id="playerAvatar">
+                <img :src="player.avatar" alt="Animated GIF">
               </div>
             </h2>
 
-  <ul>
-    <li v-for="(answer, category) in player.answers[roundCounter-1]" :key="category">
-      <div class="answerContainer">
-        <input type="checkbox" :checked="checkedAnswers[player.id] && checkedAnswers[player.id][answer]" @change="handleCheckboxChange(player.nameId, category, answer, $event)">
-        <div class="answerLabel"> {{ uiLabels[category] }}: {{ answer }} </div>
-      </div>
-      
-    </li>
-  </ul>
+            <ul>
+              <li v-for="(answer, category) in player.answers[roundCounter - 1]" :key="category">
+                <div class="answerContainer">
+                  <input type="checkbox" :checked="checkedAnswers[player.id] && checkedAnswers[player.id][answer]"
+                    @change="handleCheckboxChange(player.nameId, category, answer, $event)">
+                  <div class="answerLabel"> {{ uiLabels[category] }}: {{ answer }} </div>
+                </div>
+
+              </li>
+            </ul>
           </div>
 
         </div>
@@ -38,8 +41,8 @@
     <footer>
 
       <!--<router-link v-bind:to="'/creatorleaderboard/' + pollId">-->
-        <button id="showScoreboardButton" v-on:click="giveScore">{{uiLabels.showScoreboard}}</button>
-        <!-- göra så att man kan justera språk-->
+      <button id="showScoreboardButton" v-on:click="giveScore">{{ uiLabels.showScoreboard }}</button>
+      <!-- göra så att man kan justera språk-->
       <!--</router-link>-->
     </footer>
   </div>
@@ -48,7 +51,7 @@
 <script>
 import QuestionComponent from '@/components/QuestionComponent.vue';
 import io from 'socket.io-client';
-const socket = io(sessionStorage.getItem("dataServer")); 
+const socket = io(sessionStorage.getItem("dataServer"));
 
 export default {
   data() {
@@ -71,28 +74,28 @@ export default {
     this.pollId = this.$route.params.id;
     socket.emit("pageLoaded", this.lang);
 
-    socket.emit('startGame', {pollId: this.pollId});
+    socket.emit('startGame', { pollId: this.pollId });
 
-    socket.emit('joinSocket', {pollId: this.pollId})
+    socket.emit('joinSocket', { pollId: this.pollId })
 
     socket.on('getInfo', (poll) => {
-    console.log("getinfo", poll.players)
-     this.rounds = poll.rounds;
-     this.categories = poll.categories;
-     this.roundCounter = poll.roundCounter;
-     this.players = poll.players
-     this.selectedLetter = poll.selectedLetter;
-     
-   })
-   socket.on('getAnswers', (players) => {
+      console.log("getinfo", poll.players)
+      this.rounds = poll.rounds;
+      this.categories = poll.categories;
+      this.roundCounter = poll.roundCounter;
+      this.players = poll.players
+      this.selectedLetter = poll.selectedLetter;
+
+    })
+    socket.on('getAnswers', (players) => {
       console.log("getAnswers", players)
-     this.players = players;
-     this.isAnswered = true;
-   })
-   socket.on('movingAllToScoreboard', () => {
-    console.log("moved players to scorebaord")
-    this.$router.push('/creatorleaderboard/' + this.pollId);
-  })
+      this.players = players;
+      this.isAnswered = true;
+    })
+    socket.on('movingAllToScoreboard', () => {
+      console.log("moved players to scorebaord")
+      this.$router.push('/creatorleaderboard/' + this.pollId);
+    })
     socket.emit("pageLoaded", this.lang);
     socket.on("init", (labels) => {
       this.uiLabels = labels
@@ -105,38 +108,38 @@ export default {
     },
 
     handleCheckboxChange(playerName, category, answer, event) {
-    // Initialize this player's checked answers object if it doesn't exist yet
-    if (!this.checkedAnswers[playerName]) {
-      this.checkedAnswers = { ...this.checkedAnswers, [playerName]: {} };
-    }
-    // Update the checked state of this answer for this player
-    this.checkedAnswers = {
-      ...this.checkedAnswers,
-      [playerName]: { ...this.checkedAnswers[playerName], [answer+category]: event.target.checked },
-    };
-  },
+      // Initialize this player's checked answers object if it doesn't exist yet
+      if (!this.checkedAnswers[playerName]) {
+        this.checkedAnswers = { ...this.checkedAnswers, [playerName]: {} };
+      }
+      // Update the checked state of this answer for this player
+      this.checkedAnswers = {
+        ...this.checkedAnswers,
+        [playerName]: { ...this.checkedAnswers[playerName], [answer + category]: event.target.checked },
+      };
+    },
 
     giveScore() {
-    // Update each player's points with the count from checkedAnswers
-    this.players.forEach(player => {
-      if (this.checkedAnswers[player.nameId]) {
-        player.points = Object.values(this.checkedAnswers[player.nameId]).filter(checked => checked).length;
-      console.log(this.checkedAnswers, "poäng")
-      this.isChecked = true;
+      // Update each player's points with the count from checkedAnswers
+      this.players.forEach(player => {
+        if (this.checkedAnswers[player.nameId]) {
+          player.points = Object.values(this.checkedAnswers[player.nameId]).filter(checked => checked).length;
+          console.log(this.checkedAnswers, "poäng")
+          this.isChecked = true;
 
-      }
-    });
+        }
+      });
       console.log("giveScore", this.players, this.checkedAnswers)
       if (this.players.every(player => player.hasAnswered === true)) {
-      socket.emit('giveScore', {pollId: this.pollId, players: this.players, isChecked: this.isChecked})
-      socket.emit('moveToScoreboard', {pollId: this.pollId})
+        socket.emit('giveScore', { pollId: this.pollId, players: this.players, isChecked: this.isChecked })
+        socket.emit('moveToScoreboard', { pollId: this.pollId })
       }
       else {
         alert(this.uiLabels.everyoneHasNotAnswered);
       }
-      
 
-      
+
+
     }
   },
 };
@@ -144,34 +147,48 @@ export default {
 
 <style scoped>
 /* Add your styles here */
-
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap');
+    
+body {font-family: 'Open Sans', sans-serif;}
 .background {
   /* Stilen för att göra bilden till bakgrund på hela sidan */
-  background-image: url('/img/bluebg.png'); 
-  background-size: cover !important; /* Anpassar storleken på bilden så att den täcker hela sidan */
-  background-position: center; /* Centrerar bakgrundsbilden på sidan */
-  height: 100vh; /* Gör elementet lika högt som fönstret (hela sidan) */
+  background-image: url('/img/bluebg.png');
+  background-size: cover !important;
+  /* Anpassar storleken på bilden så att den täcker hela sidan */
+  background-position: center;
+  /* Centrerar bakgrundsbilden på sidan */
+  height: 100vh;
+  /* Gör elementet lika högt som fönstret (hela sidan) */
   margin-top: -34px;
-  
+
 }
-footer{ background: #84d4e1;}
+
+footer {
+  background: #84d4e1;
+}
+
 .wrapper {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around; /* Adjust as needed */
+  justify-content: space-around;
+  /* Adjust as needed */
 }
 
 .player-item {
-  flex: 0 0 30%; /* Adjust width as needed */
+  flex: 0 0 30%;
+  /* Adjust width as needed */
   margin: 10px;
   padding: 10px;
-  border: 2px solid black; /* Adds a black border */
-  border-radius: 10px; /* Makes corners rounded */
+  border: 2px solid black;
+  /* Adds a black border */
+  border-radius: 10px;
+  /* Makes corners rounded */
   transition: transform 1s ease-in-out;
-  background-color: rgb(240, 248, 255,0.6);
+  background-color: rgb(240, 248, 255, 0.6);
 }
-#playerAvatar img{
-  width:150px;
+
+#playerAvatar img {
+  width: 150px;
 }
 
 
@@ -183,6 +200,7 @@ footer{ background: #84d4e1;}
 .answerLabel {
   margin-right: 10px;
 }
+
 /*
 h1 {
   color: rgb(203, 60, 50);
@@ -196,6 +214,7 @@ h1 {
   from {
     opacity: 0;
   }
+
   to {
     opacity: 1;
   }
@@ -204,24 +223,31 @@ h1 {
 .title {
   margin: 0;
 }
+
 #hamster {
   height: 100px;
   width: 100px;
 }
-h1 { 
+
+h1 {
   color: rgb(249, 192, 86);
   text-shadow: rgb(255, 183, 0) 1px 0 10px;
-  font-size: 50px; 
+  font-size: 50px;
 }
-h2{font-weight: bold;
-  text-align: center;
-    color:rgb(255, 183, 0);
-    font-size: 200%}
 
-h3{font-weight: normal;
+h2 {
+  font-weight: bold;
   text-align: center;
-    color:rgb(0, 0, 0);
-    font-size: 30px}
+  color: rgb(255, 183, 0);
+  font-size: 200%
+}
+
+h3 {
+  font-weight: normal;
+  text-align: center;
+  color: rgb(0, 0, 0);
+  font-size: 30px
+}
 
 
 #showScoreboardButton {
@@ -230,8 +256,8 @@ h3{font-weight: normal;
   width: 12em;
   margin: 25px;
   position: static;
-  margin-left:88%;
-
+  margin-left: 88%;
+  font-family: 'Open Sans', sans-serif;
   border: none;
   border-radius: 10px;
 }
@@ -239,7 +265,7 @@ h3{font-weight: normal;
 #showScoreboardButton:hover {
   background-color: #70e070;
 }
+
 ul {
   list-style: none;
-}
-</style>
+}</style>
